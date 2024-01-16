b50a6f4ab8e8c1458bfeb76af10182060369fb97
benellis3 <benellis@robots.ox.ac.uk>
2024-01-16 11:08:12 +0000

    fix

--- cleanrl/ppo_atari_envpool_xla_jax_scan.py
+++ cleanrl/ppo_atari_envpool_xla_jax_scan.py
@@ -20,7 +20,7 @@ from flax.training.train_state import TrainState
 from torch.utils.tensorboard import SummaryWriter
 
 # Fix weird OOM https://github.com/google/jax/discussions/6332#discussioncomment-1279991
-os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.6"
+os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.25"
 # Fix CUDNN non-determinisim; https://github.com/google/jax/issues/4823#issuecomment-952835771
 os.environ["TF_XLA_FLAGS"] = "--xla_gpu_autotune_level=2 --xla_gpu_deterministic_reductions"
 os.environ["TF_CUDNN DETERMINISTIC"] = "1"
