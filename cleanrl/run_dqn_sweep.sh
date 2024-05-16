6d4d3561be431a3b988013ccfed27a68e8e54a58
benellis3 <benellis@robots.ox.ac.uk>
2024-05-16 17:47:25 +0100

    Fix run script

--- cleanrl/run_dqn_sweep.sh
+++ cleanrl/run_dqn_sweep.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-command=$(awk "NR==${SLURM_ARRAY_TASK_ID}" dqn_atari_10_run.txt)
+command=$(awk "NR==${SLURM_ARRAY_TASK_ID}" dqn_atari_adammr_run.txt)
 echo "$command"
 eval "$command" 
